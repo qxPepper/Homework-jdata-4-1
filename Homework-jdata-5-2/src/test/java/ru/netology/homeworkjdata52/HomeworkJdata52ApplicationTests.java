5252731f63bdf924abaec46e6cbd3621efbdc811
Vitaly Gromov <vitaly.gro@gmail.com>
2022-03-05 14:34:43 +0300

    Первоначальная версия Homework-jdata-5-2 в ветке jpa-repository-query в Homework-jdata-4-1

---
+++ Homework-jdata-5-2/src/test/java/ru/netology/homeworkjdata52/HomeworkJdata52ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.homeworkjdata52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkJdata52ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
